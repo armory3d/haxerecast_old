35d78f7547adc2ced2c2cc5545a0d1a509b7b46d
Lubos Lenco <lubos.lenco@gmail.com>
2016-12-06 19:23:41 +0100

    JS essentials

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # haxerecast
+
+- [recastnavigation](https://github.com/recastnavigation/recastnavigation)
+- [recast.js](https://github.com/vincent/recast.js)
--- Sources/haxerecast/Recast.hx
+++ Sources/haxerecast/Recast.hx
@@ -1,6 +1,26 @@
-// Haxe Recast
-// Native Recast Navigation for Haxe
+// Recast Navigation for Haxe
 // https://github.com/recastnavigation/recastnavigation
 // https://github.com/vincent/recast.js
 
 package haxerecast;
+
+#if js
+@:native('recast')
+extern class Recast {
+	// untyped recast.cb()
+	public function OBJDataLoader(data:String, done:Void->Void):Void;
+	public function buildSolo():Void;
+	public function buildTiled():Void;
+	public function getRandomPoint(callback:Dynamic):Void;
+	// callback:Float->Float->Float
+	public function findPath(from_x:Float, from_y:Float, from_z:Float, to_x:Float, to_y:Float, to_z:Float, max:Int, callback:Dynamic):Void;
+	// callback:Array<{x, y, z}>
+	public function addAgent(options:Dynamic):Void;
+}
+#end
+
+typedef RecastWaypoint = {
+	public var x:Float;
+	public var y:Float;
+	public var z:Float;
+}
