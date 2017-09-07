1c7f8dbc64fdfa84d24eccc14a7665ee4b37a34f
Lubos Lenco <lubos.lenco@gmail.com>
2017-09-07 09:59:33 +0200

    Fix include paths

--- korefile.js
+++ korefile.js
@@ -1,16 +1,16 @@
 let project = new Project('haxerecast', __dirname);
 
 project.addFile('cpp/Recast/**');
-project.addIncludeDir('cpp/Recast/include');
+project.addIncludeDir('cpp/Recast/Include');
 
 project.addFile('cpp/Detour/**');
-project.addIncludeDir('cpp/Detour/include');
+project.addIncludeDir('cpp/Detour/Include');
 
 project.addFile('cpp/DetourCrowd/**');
-project.addIncludeDir('cpp/DetourCrowd/include');
+project.addIncludeDir('cpp/DetourCrowd/Include');
 
 project.addFile('cpp/DetourTileCache/**');
-project.addIncludeDir('cpp/DetourTileCache/include');
+project.addIncludeDir('cpp/DetourTileCache/Include');
 
 project.addFile('cpp/HaxeRecast/**');
 project.addIncludeDir('cpp/HaxeRecast');
